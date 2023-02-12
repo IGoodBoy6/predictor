7359c808d22e656e167c14cc0e6c9092ccc8db6f
Neil <wangweili457@gmail.com>
2023-02-12 18:10:32 +0800

    ci: add annotations

---
+++ frameworks/gateways.core/src/main/java/ltd/highsoft/frameworks/gateways/core/AntiCorruptionLayer.java
@@ -0,0 +1,15 @@
+package ltd.highsoft.frameworks.gateways.core;
+
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface AntiCorruptionLayer {
+}
--- src/test/java/ltd/highsoft/predictor/infra/PingApi.java
+++ src/test/java/ltd/highsoft/predictor/infra/PingApi.java
@@ -1,13 +1,14 @@
 package ltd.highsoft.predictor.infra;
 
 import ltd.highsoft.frameworks.domain.core.ValueSink;
+import ltd.highsoft.frameworks.gateways.core.OpenHostService;
 import ltd.highsoft.frameworks.payload.core.Payload;
 import org.springframework.web.bind.annotation.*;
 
 import static ltd.highsoft.frameworks.domain.core.GlobalValueSinkFactory.createValueSink;
 import static ltd.highsoft.frameworks.payload.core.StringFieldType.asString;
 
-@RestController
+@OpenHostService
 public class PingApi {
 
     @PostMapping("/test/ping")
