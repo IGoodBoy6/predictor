8aa4dca5c9255478b485e24ad102bd215d436742
Neil <wangweili457@gmail.com>
2023-02-13 22:03:29 +0800

    refactor: rename DocumentsApi

--- modules/basic.gateways/src/main/java/ltd/highsoft/predictor/basic/gateways/ohs/DocumentsApi.java
+++ modules/basic.gateways/src/main/java/ltd/highsoft/predictor/basic/gateways/ohs/DocumentsApi.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DocumentsApi {
 
     @RequestMapping
-    public String test() {
+    public String docs() {
         return "apidoc.html";
     }
 
