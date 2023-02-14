a6e1204bd1d9fc030691696e8265d0b1ed1a5494
Neil <wangweili457@gmail.com>
2023-02-14 19:59:45 +0800

    feat: add user domain

---
+++ modules/basic.core/src/main/java/ltd/highsoft/predictor/basic/core/User.java
@@ -0,0 +1,29 @@
+package ltd.highsoft.predictor.basic.core;
+
+import ltd.highsoft.frameworks.domain.core.fields.*;
+
+import java.util.Optional;
+
+public class User {
+
+    private final Id id;
+    private final Name username;
+    private final Name name;
+    private final Email email;
+    private final Optional<String> biography;
+    private final Optional<String> url;
+    private final Optional<String> organization;
+    private final Optional<String> location;
+
+    public User(Id id, Name username, Name name, Email email, Optional<String> biography, Optional<String> url, Optional<String> organization, Optional<String> location) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.biography = biography;
+        this.url = url;
+        this.organization = organization;
+        this.location = location;
+    }
+
+}
---
+++ modules/basic.core/src/test/java/ltd/highsoft/predictor/basic/core/UserTest.java
@@ -0,0 +1,19 @@
+package ltd.highsoft.predictor.basic.core;
+
+import ltd.highsoft.frameworks.domain.core.fields.*;
+import ltd.highsoft.frameworks.test.context.WithId;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WithId("1")
+class UserTest {
+
+    @Test
+    void should_create_a_user() {
+        assertThat(new User(new Id(), new Name(""), new Name(""), new Email(""), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty())).isNotNull();
+    }
+
+}
